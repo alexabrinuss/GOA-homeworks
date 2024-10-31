089d4c3695ce726084311bee8bea7253e95f59c7
alexabrinuss <alexsandrinus17@gmail.com>
2024-10-31 17:19:02 +0400

    commit

---
+++ LEVEL 05/GOA HOMEWORK.py
@@ -0,0 +1,122 @@
+a = '1'
+b = '2'
+g = '3'
+d = '4'
+e = '5'
+v = '6'
+z = '7'
+t = '8'
+i = '9'
+k = '10'
+l = '11'
+m = '12'
+n = '13'
+o = '14'
+p = '15'
+j = '16'
+r = '17'
+s = '18'
+T = '19'
+u = '20'
+
+print (a)
+print (b)
+print (g)
+print (d)
+print (e)
+print (v)
+print (z)
+print (t)
+print (i)
+print (k)
+print (l) 
+print (m)
+print (n)
+print (o)
+print (p)
+print (j)
+print (r)
+print (s)
+print (T)
+print (u)
+
+GOA = 'GOA'
+Best = 'Best'
+
+K  =  GOA  + " " +  Best
+
+print ( K )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
